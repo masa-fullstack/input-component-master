f411cd4ec4eaa9b12ff06e61a02c3fdea054e480
masa-fullstack <ishida.masahidee@gmail.com>
2021-03-04 14:38:19 +0900

    responsive

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -17,39 +17,39 @@ const Home = (): JSX.Element => (
         <Label>{`<Input disabled />`}</Label>
         <Input disabled />
       </div>
-      <div>
+      <div className="md:col-span-1 col-span-3">
         <Label>{`<Input helperText=”Some interesting text” />`}</Label>
         <Input helperText="Some interesting text" />
       </div>
-      <div>
+      <div className="md:col-span-2 col-span-3">
         <Label>{`<Input helperText=”Some interesting text” error />`}</Label>
         <Input helperText="Some interesting text" error />
       </div>
-      <div className="col-start-1">
+      <div className="md:col-span-1 col-span-3">
         <Label>{`<Input startIcon="phone" />`}</Label>
         <Input startIcon="phone" />
       </div>
-      <div>
+      <div className="md:col-span-2 col-span-3">
         <Label>{`<Input endIcon="lock" />`}</Label>
         <Input endIcon="lock" />
       </div>
-      <div className="col-start-1">
+      <div className="col-span-3">
         <Label>{`<Input value="text" />`}</Label>
         <Input value="text" />
       </div>
-      <div className="col-start-1">
+      <div className="md:col-span-1 col-span-3">
         <Label>{`<Input size="sm" />`}</Label>
         <Input size="sm" />
       </div>
-      <div>
+      <div className="md:col-span-1 col-span-3">
         <Label>{`<Input size="md" />`}</Label>
         <Input size="md" />
       </div>
-      <div className="col-start-1 col-span-3">
+      <div className="col-span-3">
         <Label>{`<Input fullWidth />`}</Label>
         <Input fullWidth />
       </div>
-      <div className="col-start-1 col-span-3">
+      <div className="col-span-3">
         <Label>{`<Input multiline row="4" />`}</Label>
         <Input multiline row="4" />
       </div>
